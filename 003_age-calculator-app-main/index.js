b67e91df7f47300f34057b49138a97fcf64091aa
Giovanni Brienza <gio.brienza@gmail.com>
2023-04-10 11:47:11 +0200

    Made app responsive. Fixed date validation.

--- 003_age-calculator-app-main/index.js
+++ 003_age-calculator-app-main/index.js
@@ -1,15 +1,19 @@
 const submitButton = document.getElementsByTagName("form").item(0);
 const inputs = document.querySelectorAll("input");
 
+// Checks if Date is valid. Returns false if not.
 function isValidDate(day, month, year) {
   const date = new Date(year, month - 1, day);
-  return (
-    date.getFullYear() === year &&
-    date.getMonth() === month - 1 &&
-    date.getDate() === day
-  );
+  if (date < Date.now()) {
+    return (
+      date.getFullYear() === year &&
+      date.getMonth() === month - 1 &&
+      date.getDate() === day
+    );
+  } else return false;
 }
 
+// Calculates age based on year, month and day passed to this function.
 function calculateAge(yearI, monthI, dayI) {
   const today = new Date();
   const birthDateObj = new Date(yearI, monthI - 1, dayI);
@@ -39,10 +43,13 @@ submitButton.addEventListener("submit", function (e) {
   let monthval = parseInt(document.querySelector(".month-i input").value);
   let yearval = parseInt(document.querySelector(".year-i input").value);
 
+  // Variables that will be passed to the validation function and then to the calculator function
+  // if none of them is null
   let validatedDay = null;
   let validatedMonth = null;
   let validatedYear = null;
 
+  //Check if day number is not null, less than 1 or more than 31
   if (isNaN(dayval) || dayval > 31 || dayval < 1) {
     if (isNaN(dayval)) {
       document.querySelector(".day-i span").innerHTML =
@@ -55,12 +62,13 @@ submitButton.addEventListener("submit", function (e) {
     document.querySelector(".day-i label").style.color = "hsl(0, 100%, 67%)";
   } else {
     document.querySelector(".day-i input").style.border =
-      "1px solid hsl(0, 1%, 44%)";
+      "1px solid hsl(0, 0%, 86%)";
     document.querySelector(".day-i label").style.color = "hsl(0, 1%, 44%)";
     document.querySelector(".day-i span").innerHTML = "";
     validatedDay = dayval;
   }
 
+  //Check if month number is not null, less than 1 or more than 12
   if (isNaN(monthval) || monthval > 12 || monthval < 1) {
     if (isNaN(monthval)) {
       document.querySelector(".month-i span").innerHTML =
@@ -73,7 +81,7 @@ submitButton.addEventListener("submit", function (e) {
     document.querySelector(".month-i label").style.color = "hsl(0, 100%, 67%)";
   } else {
     document.querySelector(".month-i input").style.border =
-      "1px solid hsl(0, 1%, 44%)";
+      "1px solid hsl(0, 0%, 86%)";
     document.querySelector(".month-i label").style.color = "hsl(0, 1%, 44%)";
     document.querySelector(".month-i span").innerHTML = "";
     validatedMonth = monthval;
@@ -81,6 +89,7 @@ submitButton.addEventListener("submit", function (e) {
 
   let date = new Date();
 
+  //Check if day number is not null, less than 1 or more than current year
   if (isNaN(yearval) || yearval > date.getFullYear() || yearval < 1) {
     if (isNaN(yearval)) {
       document.querySelector(".year-i span").innerHTML =
@@ -95,19 +104,15 @@ submitButton.addEventListener("submit", function (e) {
     document.querySelector(".year-i label").style.color = "hsl(0, 100%, 67%)";
   } else {
     document.querySelector(".year-i input").style.border =
-      "1px solid hsl(0, 1%, 44%)";
+      "1px solid hsl(0, 0%, 86%)";
     document.querySelector(".year-i label").style.color = "hsl(0, 1%, 44%)";
     document.querySelector(".year-i span").innerHTML = "";
     validatedYear = yearval;
   }
 
-  console.log(validatedDay + " " + validatedMonth + " " + validatedYear);
+  // If numbers are valid checks if date is valid and then passes them to the age calculator
 
-  if (
-    validatedDay !== null &&
-    validatedMonth !== null &&
-    validatedYear !== null
-  ) {
+  if (validatedDay && validatedMonth && validatedYear) {
     if (isValidDate(validatedDay, validatedMonth, validatedYear)) {
       document.querySelector(".day-i span").innerHTML = "";
       let age = calculateAge(validatedYear, validatedMonth, validatedDay);
