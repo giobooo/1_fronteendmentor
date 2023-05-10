d41edd3c17d6cb9511086f884f3893a054f6f086
Giovanni Brienza <gio.brienza@gmail.com>
2023-05-10 13:32:03 +0200

    desktop view finished

--- 014_bmi-calculator/src/main.tsx
+++ 014_bmi-calculator/src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import HeaderComponent from './components/header.tsx'
 import "./styles/index.scss"
 import MeaningSection from './components/result-meaning.tsx'
+import TipsSection from './components/tips-section.tsx'
+import Limitations from './components/limitations-section.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HeaderComponent />
     <MeaningSection></MeaningSection>
+    <TipsSection></TipsSection>
+    <Limitations></Limitations>
   </React.StrictMode>,
 )
